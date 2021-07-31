ec7f8da565095dfad52c10714dfc79aff3f04145
hblockx <mirko.nell@gmail.com>
2021-07-31 09:32:24 +0200

    try new

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -3,7 +3,7 @@
 # All rights reserved
 
 # Variables.
-FVTT_VERSION=0.7.9
+FVTT_VERSION=0.8.8
 
 echo " "
 echo "Current FoundryVTT Release version is ${FVTT_VERSION}."
@@ -15,12 +15,22 @@ mkdir -p /foundry/fvtt /foundry/data
 chown 99:100 -R /foundry
 chmod 776 -R /foundry
 chmod +x /foundry/
-MODULE_BACKEND_JS="/foundry/data/Data/modules/plutonium/server/0.7.x/plutonium-backend.js"
+MODULE_BACKEND_JS="/foundry/data/Data/modules/plutonium/server/0.8.x/plutonium-backend.mjs"
 cp "${MODULE_BACKEND_JS}" "/foundry/fvtt/resources/app/"
-sed --file=- --in-place=.orig /foundry/fvtt/resources/app/main.js << SED_SCRIPT
-s/^\(require(\"init\").*\);\
-/\1.then(() => {require(\"plutonium-backend\").init();});/g\
-w plutonium_patchlog.txt
+
+apk add patch
+patch --backup --quiet --batch /foundry/fvtt/resources/app/main.mjs << PATCH_FILE
+26c26
+<   init.default({
+---
+>   await init.default({
+31c31,32
+<   })
+---
+>   });
+>   (await import("./plutonium-backend.mjs")).Plutonium.init();
+PATCH_FILE
+
 SED_SCRIPT
 
 # Run.
